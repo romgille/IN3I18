8580185d84e7f69e8b6854b1a2aa5bd773a46388
Romain GILLE <rgille@somone.fr>
2017-04-14 18:38:45 +0200

    file fonctionnel

--- tp2/file.py
+++ tp2/file.py
@@ -17,12 +17,11 @@
 #   e - Effectuer le même traitement appliqué au fichier "mbox.txt" et comparez
 #       le temps de traitement de chaque fichier.
 
-
-import sys
 import re
 from time import time
 
-def writeAddressesToFile(contacts, file):
+
+def write_addresses_to_file(contacts, file):
     f = open(file, 'w')
     for address in contacts:
         f.write(address)
@@ -30,22 +29,25 @@ def writeAddressesToFile(contacts, file):
     f.close()
     return 0
 
-def findEmailAddress(file):
-    email = re.compile('([\w\.]+@[\w\.]+)')
-    emailMatch = email.findall(file)
+
+def find_email_address(file):
+    email = re.compile('([\w.]+@[\w.]+)')
+    email_match = email.findall(file)
     contact = []
-    for address in emailMatch:
+    for address in email_match:
         contact.append(re.sub('<|>', '', address))
     return contact
 
-def suppressDoubleElementInList(input):
-    newList = list(set(input))
-    return newList
 
-def storeWords(file):
+def suppress_double_element_in_list(input):
+    new_list = list(set(input))
+    return new_list
+
+
+def store_words(file):
     dic = {}
     for word in file.split():
-        if(word.isdigit()):
+        if word. isdigit():
             continue
         if word in dic.keys():
             dic[word] += 1
@@ -53,60 +55,59 @@ def storeWords(file):
             dic[word] = 1
     return dic
 
-def showMostUsedWords(dic):
-    sorted_list = sorted([(value,key) for (key,value) in dic.items()])
-    mostUsedWords = []
+
+def show_most_used_words(dic):
+    sorted_list = sorted([(value, key) for (key, value) in dic.items()])
+    most_used_words = []
     for i in range(1, 10):
-        mostUsedWords.append(sorted_list[-i][1])
-    return mostUsedWords
+        most_used_words.append(sorted_list[-i][1])
+    return most_used_words
 
-def replacePunctuation(file):
-    punctuation = re.compile('(,|;|\/|-|_|\+|,|!|#|\$|%|\^|&|\*|\(|\)|\||<|>|:)|( \w ){1}')
-    newFile = punctuation.sub(' ', file)
-    return newFile
 
-def countChar(file):
+def replace_punctuation(file):
+    punctuation = re.compile('([,;/-_+!#$%^&*()|<>:])|( \w )')
+    new_file = punctuation.sub(' ', file)
+    return new_file
+
+
+def count_char(file):
     count = 0
     for line in file:
-        for char in line:
+        for _ in line:
             count += 1
     return count
 
-def readFile(file):
+
+def read_file(file):
     return file.read()
 
-def openFileForReading(filePath):
-    f = open(filePath, 'r')
+
+def open_file_for_reading(file_path):
+    f = open(file_path, 'r')
     return f
+
 
 def main():
     start_time_short = time()
-    f = openFileForReading('./mbox_short.txt')
-    file = readFile(f)
-    count = countChar(file)
-    fileWithoutPunct = replacePunctuation(file)
-    fileCountChar = countChar(fileWithoutPunct)
-    dic = storeWords(fileWithoutPunct)
-    mostUsedWords = showMostUsedWords(dic)
-    contacts = suppressDoubleElementInList(findEmailAddress(file))
-    writeAddressesToFile(contacts, 'contactstp2_short.csv')
+    f = open_file_for_reading('./mbox_short.txt')
+    file = read_file(f)
+    contacts = suppress_double_element_in_list(find_email_address(file))
+    write_addresses_to_file(contacts, 'contactstp2_short.csv')
     end_time_short = time()
     elapsed_time_short = end_time_short - start_time_short
+    print("mbox short")
     print(elapsed_time_short)
 
     start_time_short = time()
-    f = openFileForReading('./mbox.txt')
-    file = readFile(f)
-    count = countChar(file)
-    fileWithoutPunct = replacePunctuation(file)
-    fileCountChar = countChar(fileWithoutPunct)
-    dic = storeWords(fileWithoutPunct)
-    mostUsedWords = showMostUsedWords(dic)
-    contacts = suppressDoubleElementInList(findEmailAddress(file))
-    writeAddressesToFile(contacts, 'contactstp2.csv')
+    f = open_file_for_reading('./mbox.txt')
+    file = read_file(f)
+    contacts = suppress_double_element_in_list(find_email_address(file))
+    write_addresses_to_file(contacts, 'contactstp2.csv')
     end_time_short = time()
     elapsed_time_short = end_time_short - start_time_short
+    print("mbox")
     print(elapsed_time_short)
+
 
 if __name__ == '__main__':
     main()
